e5c25ffa17e9741e94834488a9045ff9eaca39bd
tindoann <tindoann@gamil.com>
2019-06-29 23:05:17 -0400

    added comment & refactor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,139 +1,85 @@
-let randNum = 0;  //random_result; 
-let losses = 0; 
-let wins = 0; 
-let previous = 0; 
-let counter = 0; 
-let gems = ['gem1.jpg', 'gem2.jpg', 'gem3.jpg', 'gem4.jpg'];
+// set global variables 
+
+let randNum = 0;  
+let losses = 0;
+let wins = 0;
+let counter = 0;
+let images = ['assets/images/gem1.jpg', 'assets/images/gem2.jpg', 'assets/images/gem3.jpg', 'assets/images/gem4.jpg']
 
 function gameStart() {
 
-// $('.crystals').empty(); 
+  // $('.crystals').empty(); 
 
-// The random number shown at the start of the game should be between 19 - 120. 
+  // The random number shown at the start of the game should be between 19 - 120. 
 
-randNum = Math.floor(Math.random() * 120) + 19; 
+  randNum = Math.floor(Math.random() * 120) + 19;
 
-// There will be four crystals displayed as buttons on the page.
+  // There will be four crystals displayed as buttons on the page.
 
-$('#randNum').html(randNum); 
+  $('#randNum').html(randNum);
 
-// * Each crystal should have a random hidden value between 1 - 12. /
-for (let i = 0; i < 4; i++) {
+  // Each crystal should have a random hidden value between 1 - 12. /
+  for (let i = 0; i < 4; i++) {
 
-var random = Math.floor(Math.random() * 11) + 1;
-//console.log(random);
+    // Each crystal should have a random hidden value between 1 - 12.
 
-  let crystal = $('<div>'); 
-      crystal.attr({
-        'class': 'crystal', 
-        'randGem': random
-      }); 
+    let random = Math.floor(Math.random() * 11) + 1;
+    //console.log(random);
 
-  $('.crystals').append(crystal); 
+    let crystal = $('<div>');
+    crystal.attr({
+      'class': 'crystal',
+      'randGem': random
+    });
+    crystal.css({
+      "background-image": "url('" + images[i] + "')",
+      "background-size": "cover"
+    })
+
+    $('.crystals').append(crystal);
+  }
+
+  // Your game will hide this amount until the player clicks a crystal.
+
+  $('.crystal').on('click', function () {
+    console.log($(this).attr('randGem'));
+
+    var crystalValue = parseInt($(this).attr('randGem'));
+    counter += crystalValue;
+    // console.log(counter);
+
+    // The player wins if their total score matches the random number from the beginning of the game. 
+
+    if (counter == randNum) {
+      alert("You win!");
+      wins++
+      counter = 0;
+      reset();
+
+    } else if (counter > randNum) {
+      alert('You Lose!');
+      losses++;
+      counter = 0;
+      reset();
+
+    } else {
+      $('#total').html(counter);
+    }
+  });
+
 }
 
-$('.crystal').on('click', function () {
-  console.log($(this).attr('randGem')); 
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero. 
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
-  var crystalValue = parseInt($(this).attr('randGem'));
-  counter += crystalValue;
-  // console.log(counter);
-
-  if (counter == randNum) {
-    alert("You win!");
-    wins++
-    gameStart(); 
-  } else if (counter > randNum) {
-    alert('You Lose!'); 
-    losses++; 
-    gameStart(); 
-  } else {
-    $('#total').html(counter); 
-  }
-});
-
-
-
+function reset() {
+  randNum = Math.floor(Math.random() * 120) + 19;
+  $("#randNum").html(randNum);
+  $("#total").html(0);
+  $("#wins").html(wins);
+  $("#losses").html(losses);
 }
 
 gameStart();
 
-/* 
-resetAndStart(); 
 
-
-/* $('crystal').on('click', function () {
-  console.log($(this)); 
-
-$(document).on('click', '.crystal', function () {
-  
-  var num = parseInt($(this).attr('data-random')); 
-
-  previous += num; 
-
-  console.log(previous); 
-
-  $('#previous').html(previous); 
-  console.log(previous);
-
-  if(previous > randNum) {
-    console.log('You lost!!'); 
-
-    lost--; 
-    $('#lost').html('You lost: ' + lost); ; 
-    
-    previouis = 0; 
-  
-    resetAndStart(); 
-  }
-  else if(previous === randNum){
-    console.log('You win!!!'); 
-
-    win++;
-
-    $('#win').html('You win: ' + win); 
-
-    previous = 0; 
-
-    resetAndStart(); 
-
-  }
-}); 
-
-*/
-
-   
-
- /*    * Your game will hide this amount until the player clicks a crystal. <==
-
-   * The player wins if their total score matches the random number from the beginning of the game. 
-
-
-
-
-     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero. <== !IMPORTANT 
-
-   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
-##### Option 1 Game design notes
-
-// !IMPORTANT
-
-* Each crystal should have a random hidden value between 1 - 12. // !IMPORTANT GEM
-*/
-
-
-// pseudo-code
-
-// a game with 4 crystal and random result
-// every crystal needs to have a random number between 1 - 12
-// a new random number should be generate every single time we win or lose
-// when clicking any crystal, it should be adding with the previous number
-// until it equals the total score 
-// if it is greater than Random Result, we decrement a lost counter
-// if it is equal, then we increment a win counter
-
-
-// The player will be shown a random number at the start of the game.
-// The random number shown at the start of the game should be between 19 - 120. 
- 
